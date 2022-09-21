f64d48ceda90beee40d3186911c74054bc474d83
Tim Smart <tim.smart@arisechurch.com>
2022-09-21 22:00:18 +1200

    add simple collection test

---
+++ src/collection.test.ts
@@ -0,0 +1,22 @@
+import { describe, test, expect } from "@jest/globals";
+import { valueNotifierCollection } from "./mod";
+
+describe("valueNotifierCollection", () => {
+  test("listens to children", () => {
+    const coll = valueNotifierCollection([1, 2, 3]);
+
+    let count = 0;
+    const cancel = coll.subscribe(() => count++);
+
+    coll.value[0].set(-1);
+    coll.value[1].set(-2);
+    coll.value[2].set(-3);
+    coll.value[2].set(-3);
+
+    expect(count).toBe(3);
+
+    cancel();
+    coll.value[0].set(0);
+    expect(count).toBe(3);
+  });
+});
